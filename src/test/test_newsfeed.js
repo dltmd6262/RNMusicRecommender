7a8266279b6d4c26eaae22780adfbb487358fffe
Seung Yup Han <seungyup.han@ikoob.com>
2016-07-22 13:21:09 +0900

    add test cases for newsfeed reducer

---
+++ src/test/test_newsfeed.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+import newsfeed from '../reducers/newsfeed';
+import {UPDATE_NEWSFEED_CARDS, UPDATE_ADD_RESULT, } from '../actions/newsfeed'
+
+describe('Newsfeed reducer test', function () {
+  it('')
+})
