a4d76a842f858daeeebf0abb1d4bd86cbc117053
dltmd6262 <dltmd6262@kaist.ac.kr>
2016-11-30 20:50:53 +0900

    Get more file info

--- android/app/src/main/java/com/filesystem/FileSystem.java
+++ android/app/src/main/java/com/filesystem/FileSystem.java
@@ -1,6 +1,7 @@
 package com.filesystem;
 
-import android.os.Environment;
+import android.database.Cursor;
+import android.provider.MediaStore;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Promise;
@@ -10,9 +11,6 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.real_pitch.MainActivity;
-
-import java.io.File;
-import java.util.ArrayList;
 
 public class FileSystem extends ReactContextBaseJavaModule {
 
@@ -27,38 +25,52 @@ public class FileSystem extends ReactContextBaseJavaModule {
         return "FileSystem";
     }
 
-    void walkFileTree(File[] paths, String name) {
-        ArrayList<File> nextFolderPaths = new ArrayList<File>(0);
+    void walkFileTree() {
+        String selection = MediaStore.Audio.Media.IS_MUSIC + " != 0";
 
-        WritableMap folder = Arguments.createMap();
-        WritableArray files = Arguments.createArray();
+        String[] projection = {
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.ALBUM_ID,
+        };
 
-        folder.putString("name", name);
+        Cursor cursor = this.getReactApplicationContext().getContentResolver().query(
+                MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                projection,
+                selection,
+                null,
+                null);
 
-        for (File file : paths) {
-            if (file.isDirectory()) {
-                walkFileTree(file.listFiles(), file.getName());
-            } else {
-                String fileName = file.getName();
-                if (fileName.endsWith(".mp3")) {
-                    WritableMap currentFile = Arguments.createMap();
-                    currentFile.putString("fileName", fileName);
-                    currentFile.putString("path", file.getAbsolutePath());
-                    files.pushMap(currentFile);
-                }
+        while(cursor != null & cursor.moveToNext()) {
+            WritableMap fileDB = Arguments.createMap();
+
+            fileDB.putString("artist", cursor.getString(0));
+            fileDB.putString("title", cursor.getString(1));
+            fileDB.putString("path", cursor.getString(2));
+            fileDB.putString("fileName", cursor.getString(1));
+            fileDB.putString("duration", cursor.getString(3));
+
+            Cursor albumCur = this.getReactApplicationContext().getContentResolver().query(MediaStore.Audio.Albums.EXTERNAL_CONTENT_URI,
+                    new String[] {MediaStore.Audio.Albums.ALBUM_ART},
+                    MediaStore.Audio.Albums._ID + '=' + cursor.getString(4),
+                    null, null
+            );
+
+            if (albumCur != null && albumCur.moveToFirst()) {
+                String albumPath = albumCur.getString(albumCur.getColumnIndex(MediaStore.Audio.Albums.ALBUM_ART));
+                fileDB.putString("album", albumPath);
+                albumCur.close();
             }
+
+            this.allFiles.pushMap(fileDB);
         }
-
-        folder.putArray("files", files);
-
-        this.allFiles.pushMap(folder);
     }
 
     @ReactMethod
     public void getFoldersWithMusic(Promise promise) {
-        File root = Environment.getExternalStorageDirectory();
-        walkFileTree(new File[]{root}, "root");
-
+        walkFileTree();
         promise.resolve(this.allFiles);
     }
 
