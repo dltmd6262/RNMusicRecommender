dd6ecf248841d93d4d3d346455a7935db6b47b66
dltmd6262 <dltmd6262@kaist.ac.kr>
2017-07-05 14:06:54 +0900

    update react native version

--- android/app/src/main/java/com/real_pitch/MainActivity.java
+++ android/app/src/main/java/com/real_pitch/MainActivity.java
@@ -5,7 +5,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.animation.LinearInterpolator;
 
 import com.facebook.react.ReactActivity;
 
